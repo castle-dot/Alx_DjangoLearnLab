9362981ce3b319840628696c52c244ea1d7977e9
castle-dot <castle2wonderland@gmail.com>
2025-08-01 23:47:21 +0300

    another one

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -9,10 +9,10 @@ match priority:
         priority_message = "medium priority"
     case "low":
         priority_message = "low priority"
+if time_bound == "yes":
+    time_message = "requires immediate attention today!"
+else:
+    time_message = "Consider completing it when you have free time."
 
-match time_bound:
-    case "yes":
-        time_message = "requires immediate attention today!"
-    case "no":
-        time_message = "Consider completing it when you have free time."
+
 print(f"Reminder: '{task}' is a {priority_message} task that {time_message}")
