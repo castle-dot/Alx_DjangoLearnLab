e833a1b8a8d833202a4379dc964e9e14608a4073
castle-dot <castle2wonderland@gmail.com>
2025-08-01 23:30:43 +0300

    fixed some string problem

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
-x = input("What's the weather like today? (sunny/rainy/cold):")
-if x == 'rainy':
-    print("Don't forget your umbrella and a raincoat.")
-elif x == 'sunny':
+weather = input("What's the weather like today? (sunny/rainy/cold):")
+if weather == 'sunny':
     print("Wear a t-shirt and sunglasses.")
-elif x == 'cold':
+elif weather == 'rainy':
+    print("Don't forget your umbrella and a raincoat.")
+elif weather == 'cold':
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
