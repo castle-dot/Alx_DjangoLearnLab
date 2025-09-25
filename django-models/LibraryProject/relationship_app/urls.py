405a8e34acec9b560ef021d0df102969a831190c
castle-dot <castle2wonderland@gmail.com>
2025-09-25 15:48:49 +0300

    Done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views  # Import views from the current app
+from . import views 
+from .views import list_books, LibraryDetailView # Import views from the current app
+
+# relationship_app/urls.py
 
 urlpatterns = [
-    # Define the URL pattern for the library detail page
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    # Add any other views you need here
+    path('books/', views.list_books, name='list_books'),  # URL pattern for listing books
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL pattern for library details
 ]
+
