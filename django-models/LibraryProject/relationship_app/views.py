c316421bde37f6ace52c7f97ead60ca0b5aa0af6
castle-dot <castle2wonderland@gmail.com>
2025-09-25 15:41:35 +0300

    done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 from relationship_app.models import Book
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from relationship_app.models import Library
 from .models import Library
 
