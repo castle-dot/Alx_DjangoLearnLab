0e3e0f0731b60ae75c728994bdf2fbf91d7c62e5
castle-dot <castle2wonderland@gmail.com>
2025-09-26 11:07:37 +0300

    Done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
 
 from relationship_app.models import Book
@@ -32,3 +34,32 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+def user_is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@login_required
+@user_passes_test(user_is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def user_is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@login_required
+@user_passes_test(user_is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+def user_is_member(user):
+    return user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(user_is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+@login_required
+@user_passes_test(user_is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
