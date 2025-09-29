d26b6d13339cb70181ce3343d1e0b4453ed4ce0c
castle-dot <castle2wonderland@gmail.com>
2025-09-29 21:14:22 +0300

    Done

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Article
+from .models import Book
 
 # View to edit an article
 @permission_required('bookshelf.can_edit', raise_exception=True)
@@ -13,3 +14,8 @@ def edit_article(request, article_id):
         article.content = request.POST['content']
         article.save()
     return render(request, 'edit_article.html', {'article': article})
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # Get all books
+    return render(request, 'book_list.html', {'books': books})
