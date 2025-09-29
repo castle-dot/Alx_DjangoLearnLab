ef3600f85eaa39ddbe1a047a1dad6ce776edf20b
castle-dot <castle2wonderland@gmail.com>
2025-09-29 21:12:09 +0300

    Done

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Article
 
-# Create your views here.
+# View to edit an article
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == 'POST':
+        # Handle the form submission for editing the article
+        article.title = request.POST['title']
+        article.content = request.POST['content']
+        article.save()
+    return render(request, 'edit_article.html', {'article': article})
