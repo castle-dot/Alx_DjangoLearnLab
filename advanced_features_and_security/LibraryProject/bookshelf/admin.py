59c721e37b1af2a3c1526c0e417f5894b05de73c
castle-dot <castle2wonderland@gmail.com>
2025-09-29 20:54:22 +0300

    Done

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Book
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin 
+
+class BookAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'author', 'publication_year')
+    
+    
+    search_fields = ('title', 'author')
+    
+   
+    list_filter = ('publication_year',)
+
+admin.site.register(Book, BookAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    list_filter = ['is_staff', 'is_superuser', 'date_of_birth']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('username', 'email', 'password1', 'password2')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    search_fields = ['username', 'email']
+    ordering = ['username']
+
+admin.site.register(CustomUser, CustomUserAdmin)
