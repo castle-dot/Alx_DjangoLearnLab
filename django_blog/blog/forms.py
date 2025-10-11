5481226d820194b6bcf5282eec83f3008a4fe3d7
castle-dot <castle2wonderland@gmail.com>
2025-10-11 11:56:18 +0300

    Done

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Comment 
+from taggit.forms import TagWidget
 
 from blog.models import Post
 
@@ -16,6 +17,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
